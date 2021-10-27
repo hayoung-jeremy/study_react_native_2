3e707bec380a909b94357705a5561690bdff8622
Ha young, Kim <jeremiah91@naver.com>
2021-10-27 11:52:46 +0900

    feat : Layout and Style

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style={"light"} />
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
+      </View>
+      <View style={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </View>
     </View>
   );
 }
@@ -14,8 +22,36 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#151515",
+  },
+  city: {
+    flex: 1,
+    backgroundColor: "#222",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName: {
+    color: "#eee",
+    fontSize: 42,
+    fontWeight: "600",
+  },
+  weather: {
+    flex: 3,
+    backgroundColor: "#242424",
+  },
+  day: {
+    flex: 1,
+    backgroundColor: "#282828",
+    alignItems: "center",
+  },
+  temp: {
+    marginTop: 20,
+    fontSize: 160,
+    color: "#eee",
+  },
+  description: {
+    marginTop: -30,
+    fontSize: 60,
+    color: "#eee",
   },
 });
