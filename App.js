50863f4c0aaf3c673a21a90a98e8eac7080186ca
Ha young, Kim <jeremiah91@naver.com>
2021-10-28 15:07:23 +0900

    feat : Layout and Style --2
     - ScrollView, Dimensions

--- App.js
+++ App.js
@@ -1,6 +1,10 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Dimensions, ScrollView, StyleSheet, Text, View } from "react-native";
+
+// 기기 사이즈 가져오기
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
+console.log(SCREEN_WIDTH);
 
 export default function App() {
   return (
@@ -9,12 +13,29 @@ export default function App() {
       <View style={styles.city}>
         <Text style={styles.cityName}>Seoul</Text>
       </View>
-      <View style={styles.weather}>
+      <ScrollView
+        contentContainerStyle={styles.weather}
+        horizontal
+        pagingEnabled
+        indicatorStyle="white"
+      >
         <View style={styles.day}>
           <Text style={styles.temp}>27</Text>
           <Text style={styles.description}>Sunny</Text>
         </View>
-      </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
@@ -25,8 +46,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#151515",
   },
   city: {
-    flex: 1,
-    backgroundColor: "#222",
+    flex: 0.5,
     justifyContent: "center",
     alignItems: "center",
   },
@@ -35,13 +55,8 @@ const styles = StyleSheet.create({
     fontSize: 42,
     fontWeight: "600",
   },
-  weather: {
-    flex: 3,
-    backgroundColor: "#242424",
-  },
   day: {
-    flex: 1,
-    backgroundColor: "#282828",
+    width: SCREEN_WIDTH,
     alignItems: "center",
   },
   temp: {
