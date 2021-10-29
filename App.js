56d0adcbe8580593eef147de8cc86a4d7e0ad23b
Ha young, Kim <jeremiah91@naver.com>
2021-10-29 15:41:14 +0900

    feat : Get location
     - install library : expo-location

--- App.js
+++ App.js
@@ -1,17 +1,45 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Dimensions, ScrollView, StyleSheet, Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import * as Location from "expo-location";
 
 // 기기 사이즈 가져오기
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 console.log(SCREEN_WIDTH);
 
 export default function App() {
+  const [city, setCity] = useState("Loading...");
+  const [ok, setOk] = useState(true);
+
+  const getLocation = async () => {
+    // user 에게 위치 정보 제공 동의 요청
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    if (!granted) {
+      setOk(false);
+    }
+    // 제공 동의 시, 위치 좌표 가져오기
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+    // 가져온 위치 좌표로 도시명 가져오기
+    const location = await Location.reverseGeocodeAsync(
+      {
+        latitude,
+        longitude,
+      },
+      { useGoogleMaps: false }
+    );
+    setCity(location[0].city);
+  };
+  useEffect(() => {
+    getLocation();
+  }, []);
+
   return (
     <View style={styles.container}>
       <StatusBar style={"light"} />
       <View style={styles.city}>
-        <Text style={styles.cityName}>Seoul</Text>
+        <Text style={styles.cityName}>{city}</Text>
       </View>
       <ScrollView
         contentContainerStyle={styles.weather}
