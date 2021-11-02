735f27f912795cbdec0f86feb71305e38b65ecba
Ha young, Kim <jeremiah91@naver.com>
2021-11-02 13:52:01 +0900

    feat : Stlyes
     - Link icons with weather conditions

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ import {
 } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import * as Location from "expo-location";
+import { Fontisto } from "@expo/vector-icons";
 
 // 기기 사이즈 가져오기
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
@@ -16,6 +17,17 @@ console.log(SCREEN_WIDTH);
 
 // 이런 정보는 반드시 서버에 두어야하며, 현재는 무제한 생성가능한 무료 API_KEY 이기 때문에 편의상 application 내부에 저장
 const API_KEY = "4202a96e6da9b590dc2f1b421ceb72f6";
+
+// icons
+const icons = {
+  Clouds: "cloudy",
+  Clear: "day-sunny",
+  Atmosphere: "cloudy-gusts",
+  Rain: "rains",
+  Snow: "snow",
+  Drizzle: "rain",
+  Thunderstorm: "lightning",
+};
 
 export default function App() {
   const [city, setCity] = useState("Loading...");
@@ -64,15 +76,29 @@ export default function App() {
         indicatorStyle="white"
       >
         {days.length === 0 ? (
-          <View style={styles.day}>
-            <ActivityIndicator size="large" />
+          <View style={{ ...styles.day, alignItems: "center" }}>
+            <ActivityIndicator size="large" style={{ marginTop: 10 }} />
           </View>
         ) : (
           days.map((day, index) => (
             <View key={index} style={styles.day}>
-              <Text style={styles.temp}>
-                {parseFloat(day.temp.day).toFixed(1)}
-              </Text>
+              <View
+                style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                  width: "100%",
+                }}
+              >
+                <Text style={styles.temp}>
+                  {parseFloat(day.temp.day).toFixed(1)}
+                </Text>
+                <Fontisto
+                  name={icons[day.weather[0].main]}
+                  size={60}
+                  color="#ccc"
+                  style={{ marginTop: 40, marginLeft: 20 }}
+                />
+              </View>
               <Text style={styles.description}>{day.weather[0].main}</Text>
               <Text style={styles.subDescription}>
                 {day.weather[0].description}
@@ -102,20 +128,25 @@ const styles = StyleSheet.create({
   },
   day: {
     width: SCREEN_WIDTH,
-    alignItems: "center",
+    alignItems: "flex-start",
+    paddingHorizontal: 40,
   },
   temp: {
     marginTop: 20,
-    fontSize: 160,
+    fontSize: 120,
     color: "#eee",
+    borderTopWidth: 20,
+    borderTopColor: "#fff",
   },
   description: {
     marginTop: -10,
-    fontSize: 60,
-    color: "#eee",
+    marginLeft: 20,
+    fontSize: 40,
+    color: "#fff",
   },
   subDescription: {
     fontSize: 20,
-    color: "#fff",
+    marginLeft: 20,
+    color: "#aaa",
   },
 });
